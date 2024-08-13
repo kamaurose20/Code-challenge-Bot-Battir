f04910afd735496145234d839e71d4d0cea269ce
rosekamau <kamaurose236@gmail.com>
2024-08-13 19:11:30 +0300

    first commit

---
+++ src/components/YourBotArmy.js
@@ -0,0 +1,24 @@
+import React from "react";
+import BotCard from "./BotCard";
+
+function YourBotArmy(props) {
+  const bots = props.bots.map((bot) => {
+    return <BotCard key={bot.id} bot={bot} botFunction={props.removeBot} />;
+  });
+
+  return (
+    <div className="ui form">
+      <nav className="navbar bg-dark" data-bs-theme="dark">
+        <h1>Army bots</h1>
+      </nav>
+      <div className="ui five column grid">
+        <div className="row bot-army-row">
+          {bots}
+          {bots.length === 0 ? <h1></h1> : null}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default YourBotArmy;
